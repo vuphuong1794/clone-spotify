fc22dd6601d4a464aa0876dd12a3c834d5f5a231
vuphuong1794 <phuongvu1794@gmail.com>
2023-08-06 12:38:24 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="h-20 bg-slate-900 text-white text-center leading-[5rem] text-3xl font-semibold">
+      <i className="fa fa-spotify mr-5"></i>
+      Spotify
+    </div>
+  );
+}
